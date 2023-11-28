513a2f24d88b199cd4f26f4bf4e5e9bbd1358b36
jcruzf <jcruzf@gmail.com>
2023-11-28 16:09:03 -0600

    Deploy Vercel

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro-moon-landing.netlify.app/",
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  },
+  output: "server",
+  adapter: vercel()
+});
